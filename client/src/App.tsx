e6c608038e6754505071643e0082481483758e14
bogdanaks <bogdanaks@bk.ru>
2020-09-27 20:32:49 +0300

    Add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar/Navbar'
+import { Main } from './pages/Main/Main'
 
 export const App = () => {
     return (
         <div className="App">
-            <h1>App page</h1>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route to="/" component={Main} exact />
+                </Switch>
+            </Router>
         </div>
     )
 }
