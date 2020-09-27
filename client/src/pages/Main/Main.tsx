7a9f61e25e9654e7a6fd1271e41564bee7f17286
bogdanaks <bogdanaks@bk.ru>
2020-09-27 22:15:38 +0300

    Base Main

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
-export const Main = () => {
+import styles from './styles.module.scss'
+import pizzaImg from '../../assets/pizza1.png'
+
+import { Categories } from '../../components/Categories/Categories'
+import { Filters } from '../../components/Filters/Filters'
+
+export const Main: React.FC = () => {
+    const pizzasArr = Array(5).fill(1)
     return (
-        <div>
-            <h1>Main page</h1>
+        <div className={styles.wrapper}>
+            <div className={styles.settings}>
+                <Categories />
+                <Filters />
+            </div>
+            <div className={styles.pizzas}>
+                <h1>Все</h1>
+                <div className={styles.pizzasList}>
+                    <ul>
+                        {pizzasArr.map(() => (
+                            <li>
+                                <img src={pizzaImg} alt="Pizza" />
+                                <h2>Чизбургер-пицца</h2>
+                                <div className={styles.settingsPizza}>
+                                    <div className={styles.dough}>
+                                        <button>тонкое</button>
+                                        <button>традиционное</button>
+                                    </div>
+                                    <div className={styles.size}>
+                                        <button>26 см.</button>
+                                        <button>30 см.</button>
+                                        <button>40 см.</button>
+                                    </div>
+                                </div>
+                                <div className={styles.price}>
+                                    <h2>от 395 ₽</h2>
+                                    <button>
+                                        <FontAwesomeIcon icon={faPlus} />
+                                        Добавить
+                                        <span>2</span>
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
