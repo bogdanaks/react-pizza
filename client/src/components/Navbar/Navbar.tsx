3a1e3adc255c899aec9e74da80816444a7643f2c
bogdanaks <bogdanaks@bk.ru>
2020-09-28 23:53:55 +0300

    Fix

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,22 +5,27 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import logo from '../../assets/logo.png'
 
 import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
 
 export const Navbar: React.FC = () => {
     return (
         <>
             <div className={styles.navbar}>
-                <div className={styles.logo}>
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className={styles.logoText}>
-                    <h1>REACT PIZZA</h1>
-                    <p>самая вкусная пицца во вселенной</p>
-                </div>
+                <Link to="/" className={styles.logoBlock}>
+                    <div className={styles.logo}>
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className={styles.logoText}>
+                        <h1>REACT PIZZA</h1>
+                        <p>самая вкусная пицца во вселенной</p>
+                    </div>
+                </Link>
                 <div className={styles.cart}>
-                    <button>
-                        520 ₽ <span>|</span> <FontAwesomeIcon icon={faShoppingCart} /> 3
-                    </button>
+                    <Link to="/cart">
+                        <button>
+                            520 ₽ <span>|</span> <FontAwesomeIcon icon={faShoppingCart} /> 3
+                        </button>
+                    </Link>
                 </div>
             </div>
             <div className={styles.line} />
