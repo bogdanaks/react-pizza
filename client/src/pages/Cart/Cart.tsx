2a1b2e36d367d695ab75b8a0fecf342fa606e770
bogdanaks <bogdanaks@bk.ru>
2020-09-28 23:54:04 +0300

    Fix

--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -4,112 +4,129 @@ import { faShoppingCart, faMinus, faPlus, faTimes } from '@fortawesome/free-soli
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons'
 
 import pizzaImg from '../../assets/pizza1.png'
+import emptyCart from '../../assets/emptyCart.png'
 
 import styles from './styles.module.scss'
 
 export const Cart: React.FC = () => {
+    const [empty, setEmpty] = React.useState<Boolean>(true)
     return (
         <div className={styles.cartWrapper}>
-            <div className={styles.cart}>
-                <div className={styles.header}>
-                    <FontAwesomeIcon icon={faShoppingCart} />
-                    <h1>Корзина</h1>
-                    <div className={styles.clear}>
-                        <FontAwesomeIcon icon={faTrashAlt} />
-                        <p>Очистить корзину</p>
+            {!empty && (
+                <div className={styles.cart}>
+                    <div className={styles.header}>
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        <h1>Корзина</h1>
+                        <div className={styles.clear}>
+                            <FontAwesomeIcon icon={faTrashAlt} />
+                            <p>Очистить корзину</p>
+                        </div>
+                    </div>
+                    <div className={styles.line} />
+                    <ul>
+                        <li>
+                            <div className={styles.leftBlock}>
+                                <img src={pizzaImg} alt="Pizza" />
+                                <div className={styles.pizzaInfo}>
+                                    <h3>Сырный цыпленок</h3>
+                                    <p>тонкое тесто, 26 см.</p>
+                                </div>
+                            </div>
+                            <div className={styles.rightBlock}>
+                                <div className={styles.count}>
+                                    <div className={styles.minus}>
+                                        <FontAwesomeIcon icon={faMinus} />
+                                    </div>
+
+                                    <h3>2</h3>
+                                    <div className={styles.plus}>
+                                        <FontAwesomeIcon icon={faPlus} />
+                                    </div>
+                                </div>
+                                <h2>770 ₽ </h2>
+                                <div className={styles.cross}>
+                                    <FontAwesomeIcon icon={faTimes} />
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className={styles.leftBlock}>
+                                <img src={pizzaImg} alt="Pizza" />
+                                <div className={styles.pizzaInfo}>
+                                    <h3>Сырный цыпленок</h3>
+                                    <p>тонкое тесто, 26 см.</p>
+                                </div>
+                            </div>
+                            <div className={styles.rightBlock}>
+                                <div className={styles.count}>
+                                    <div className={styles.minus}>
+                                        <FontAwesomeIcon icon={faMinus} />
+                                    </div>
+
+                                    <h3>2</h3>
+                                    <div className={styles.plus}>
+                                        <FontAwesomeIcon icon={faPlus} />
+                                    </div>
+                                </div>
+                                <h2>770 ₽ </h2>
+                                <div className={styles.cross}>
+                                    <FontAwesomeIcon icon={faTimes} />
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className={styles.leftBlock}>
+                                <img src={pizzaImg} alt="Pizza" />
+                                <div className={styles.pizzaInfo}>
+                                    <h3>Сырный цыпленок</h3>
+                                    <p>тонкое тесто, 26 см.</p>
+                                </div>
+                            </div>
+                            <div className={styles.rightBlock}>
+                                <div className={styles.count}>
+                                    <div className={styles.minus}>
+                                        <FontAwesomeIcon icon={faMinus} />
+                                    </div>
+
+                                    <h3>2</h3>
+                                    <div className={styles.plus}>
+                                        <FontAwesomeIcon icon={faPlus} />
+                                    </div>
+                                </div>
+                                <h2>770 ₽ </h2>
+                                <div className={styles.cross}>
+                                    <FontAwesomeIcon icon={faTimes} />
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className={styles.orderCount}>
+                        <span>
+                            Всего пицц: <b>3 шт.</b>
+                        </span>
+                        <h3>
+                            Сумма заказа: <span>900 ₽</span>
+                        </h3>
+                    </div>
+                    <div className={styles.buttons}>
+                        <button className={styles.btnBack}>Вернуться назад</button>
+                        <button className={styles.btnPay}>Оплатить сейчас</button>
                     </div>
                 </div>
-                <div className={styles.line} />
-                <ul>
-                    <li>
-                        <div className={styles.leftBlock}>
-                            <img src={pizzaImg} alt="Pizza" />
-                            <div className={styles.pizzaInfo}>
-                                <h3>Сырный цыпленок</h3>
-                                <p>тонкое тесто, 26 см.</p>
-                            </div>
-                        </div>
-                        <div className={styles.rightBlock}>
-                            <div className={styles.count}>
-                                <div className={styles.minus}>
-                                    <FontAwesomeIcon icon={faMinus} />
-                                </div>
+            )}
 
-                                <h3>2</h3>
-                                <div className={styles.plus}>
-                                    <FontAwesomeIcon icon={faPlus} />
-                                </div>
-                            </div>
-                            <h2>770 ₽ </h2>
-                            <div className={styles.cross}>
-                                <FontAwesomeIcon icon={faTimes} />
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.leftBlock}>
-                            <img src={pizzaImg} alt="Pizza" />
-                            <div className={styles.pizzaInfo}>
-                                <h3>Сырный цыпленок</h3>
-                                <p>тонкое тесто, 26 см.</p>
-                            </div>
-                        </div>
-                        <div className={styles.rightBlock}>
-                            <div className={styles.count}>
-                                <div className={styles.minus}>
-                                    <FontAwesomeIcon icon={faMinus} />
-                                </div>
-
-                                <h3>2</h3>
-                                <div className={styles.plus}>
-                                    <FontAwesomeIcon icon={faPlus} />
-                                </div>
-                            </div>
-                            <h2>770 ₽ </h2>
-                            <div className={styles.cross}>
-                                <FontAwesomeIcon icon={faTimes} />
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.leftBlock}>
-                            <img src={pizzaImg} alt="Pizza" />
-                            <div className={styles.pizzaInfo}>
-                                <h3>Сырный цыпленок</h3>
-                                <p>тонкое тесто, 26 см.</p>
-                            </div>
-                        </div>
-                        <div className={styles.rightBlock}>
-                            <div className={styles.count}>
-                                <div className={styles.minus}>
-                                    <FontAwesomeIcon icon={faMinus} />
-                                </div>
-
-                                <h3>2</h3>
-                                <div className={styles.plus}>
-                                    <FontAwesomeIcon icon={faPlus} />
-                                </div>
-                            </div>
-                            <h2>770 ₽ </h2>
-                            <div className={styles.cross}>
-                                <FontAwesomeIcon icon={faTimes} />
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <div className={styles.orderCount}>
-                    <span>
-                        Всего пицц: <b>3 шт.</b>
-                    </span>
-                    <h3>
-                        Сумма заказа: <span>900 ₽</span>
-                    </h3>
+            {empty && (
+                <div className={styles.emptyCart}>
+                    <h1>Корзина пустая </h1>
+                    <p>
+                        Вероятней всего, вы не заказывали ещё пиццу.
+                        <br />
+                        Для того, чтобы заказать пиццу, перейди на главную страницу.
+                    </p>
+                    <img src={emptyCart} alt="Empty cart" />
+                    <button>Вернуться назад</button>
                 </div>
-                <div className={styles.buttons}>
-                    <button className={styles.btnBack}>Вернуться назад</button>
-                    <button className={styles.btnPay}>Оплатить сейчас</button>
-                </div>
-            </div>
+            )}
         </div>
     )
 }
