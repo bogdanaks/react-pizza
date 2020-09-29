112de6cb9de3d307018b6635d921184c6d3dafd0
bogdanaks <bogdanaks@bk.ru>
2020-09-29 12:29:16 +0300

    Add pizza reducer

--- client/src/redux/reducers/rootReducer.ts
+++ client/src/redux/reducers/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import { appReducer } from './appReducer'
+import { pizzaReducer } from './pizzaReducer'
 
 export const rootReducer = combineReducers({
     app: appReducer,
+    pizza: pizzaReducer,
 })
