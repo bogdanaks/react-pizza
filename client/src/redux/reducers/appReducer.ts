e6afffb4bb580ab3dd4bf5ddc0b96be1a28bdcff
bogdanaks <bogdanaks@bk.ru>
2020-09-29 12:17:59 +0300

    Create base app reducer

---
+++ client/src/redux/reducers/appReducer.ts
@@ -0,0 +1,21 @@
+import produce, { Draft } from 'immer'
+import { APP } from '../actions/appAction/types'
+
+interface AppState {
+    app: Boolean
+}
+
+const initialState: AppState = {
+    app: true,
+}
+
+export const appReducer = produce((draft: Draft<AppState>, action) => {
+    switch (action.type) {
+        case APP.SHOW_ALERT:
+            draft.app = false
+            break
+        case APP.HIDE_ALERT:
+            draft.app = true
+            break
+    }
+}, initialState)
