ab98aaf0943e675d82f95e7d8d48849eb7b83d4c
bogdanaks <bogdanaks@bk.ru>
2020-09-29 20:20:31 +0300

    Change fetch requst

--- client/src/redux/actions/pizzaActions.ts
+++ client/src/redux/actions/pizzaActions.ts
@@ -1,12 +1,9 @@
 import { PizzaTypes, PizzaThunk } from '../types/pizzaTypes'
-// import api from 'axios'
+import api from 'axios'
 
 export const fetchPizzas = (): PizzaThunk => {
     return async (dispatch) => {
-        const pizzas = await (
-            await fetch('https://jsonplaceholder.typicode.com/posts?_limit=8')
-        ).json()
-
-        dispatch({ type: PizzaTypes.FETCH_PIZZAS, payload: pizzas })
+        const { data } = await api.get('/api/pizza')
+        dispatch({ type: PizzaTypes.FETCH_PIZZAS, payload: data })
     }
 }
