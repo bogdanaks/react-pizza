afe12578795a89b040ea349a1514ac38e016d9d3
bogdanaks <bogdanaks@bk.ru>
2020-09-29 17:56:45 +0300

    Change app types

--- client/src/redux/types/appTypes.ts
+++ client/src/redux/types/appTypes.ts
@@ -1,4 +1,10 @@
-export enum APP {
+// enums
+export enum AppTypes {
     SHOW_ALERT = 'APP/SHOW_ALERT',
     HIDE_ALERT = 'APP/HIDE_ALERT',
+}
+
+// initialState
+export interface AppState {
+    app: boolean
 }
