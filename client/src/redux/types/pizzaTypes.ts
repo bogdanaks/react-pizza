923a670366977435c6534d0a6ff0d13327b51088
bogdanaks <bogdanaks@bk.ru>
2020-09-29 12:27:51 +0300

    Refactoring

--- client/src/redux/types/pizzaTypes.ts
+++ client/src/redux/types/pizzaTypes.ts
@@ -1,7 +1,14 @@
+// enums
 export enum PIZZA {
     FETCH_PIZZAS = 'PIZZA/FETCH_PIZZAS',
 }
 
+// initialState
+export interface PizzaState {
+    pizzas: Array<object>
+}
+
+// actions
 interface FetchPizzaAction {
     type: typeof PIZZA.FETCH_PIZZAS
     payload: Array<object>
