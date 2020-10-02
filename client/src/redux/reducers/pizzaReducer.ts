1830140634acd428ad6820a293388b5d4baed555
bogdanaks <bogdanaks@bk.ru>
2020-10-02 14:47:39 +0300

    Add reduce to set categories

--- client/src/redux/reducers/pizzaReducer.ts
+++ client/src/redux/reducers/pizzaReducer.ts
@@ -3,12 +3,22 @@ import { PizzaState, PizzaTypes, PizzaActionTypes } from '../types/pizzaTypes'
 
 const initialState: PizzaState = {
     pizzas: [],
+    category: {
+        list: [],
+        active: '',
+    },
 }
 
 export const pizzaReducer = produce((draft: Draft<PizzaState>, action: PizzaActionTypes) => {
     switch (action.type) {
         case PizzaTypes.FETCH_PIZZAS:
             draft.pizzas = action.payload
+            break
+        case PizzaTypes.FETCH_CATEGORY:
+            draft.category.list = action.payload
+            break
+        case PizzaTypes.SET_CATEGORY:
+            draft.category.active = action.payload
             break
     }
 }, initialState)
