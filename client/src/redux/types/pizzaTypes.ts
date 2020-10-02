f3186e840e37eaf564bf1c231ddfdbce8484eb74
bogdanaks <bogdanaks@bk.ru>
2020-10-02 14:47:49 +0300

    Add type for category

--- client/src/redux/types/pizzaTypes.ts
+++ client/src/redux/types/pizzaTypes.ts
@@ -4,6 +4,8 @@ import { ThunkAction } from 'redux-thunk'
 // enums
 export enum PizzaTypes {
     FETCH_PIZZAS = 'PIZZA/FETCH_PIZZAS',
+    FETCH_CATEGORY = 'PIZZA/FETCH_CATEGORY',
+    SET_CATEGORY = 'PIZZA/SET_CATEGORY',
 }
 
 // initialState
@@ -11,8 +13,17 @@ export interface PizzaItem {
     _id: string
     title: string
 }
+export interface CategoryItem {
+    _id: string
+    name: string
+}
+
 export interface PizzaState {
     pizzas: PizzaItem[]
+    category: {
+        list: CategoryItem[]
+        active: string
+    }
 }
 
 //thunk type
@@ -28,5 +39,13 @@ interface FetchPizzaAction {
     type: typeof PizzaTypes.FETCH_PIZZAS
     payload: PizzaItem[]
 }
+interface FetchCategoriesAction {
+    type: typeof PizzaTypes.FETCH_CATEGORY
+    payload: CategoryItem[]
+}
+interface SetCategoryAction {
+    type: typeof PizzaTypes.SET_CATEGORY
+    payload: string
+}
 
-export type PizzaActionTypes = FetchPizzaAction
+export type PizzaActionTypes = FetchPizzaAction | FetchCategoriesAction | SetCategoryAction
